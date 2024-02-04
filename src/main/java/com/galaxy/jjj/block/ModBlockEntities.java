273204e8768dda5ec95d959b0ce1aa33ad232ae9
Tyler Holden <HoldenTyler8654@gmail.com>
2024-02-04 01:24:48 -0600

    fix InventoryConnectorEntity

--- src/main/java/com/galaxy/jjj/block/ModBlockEntities.java
+++ src/main/java/com/galaxy/jjj/block/ModBlockEntities.java
@@ -4,11 +4,13 @@ import com.galaxy.jjj.JoesJimpleJorage;
 import com.galaxy.jjj.block.inventory_connector.InventoryConnectorEntity;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
+
 public class ModBlockEntities {
-	public static final BlockEntityType<InventoryConnectorEntity> INVENTORY_CONNECTOR_ENTITY = Registry.register(Registries.BLOCK_ENTITY_TYPE,new ResourceLocation(JoesJimpleJorage.MOD_ID, "inventory_connector_entity"),
+	public static final BlockEntityType<InventoryConnectorEntity> INVENTORY_CONNECTOR_ENTITY = Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE,new ResourceLocation(JoesJimpleJorage.MOD_ID, "inventory_connector_entity"),
 			FabricBlockEntityTypeBuilder.create(InventoryConnectorEntity::new,ModBlocks.INVENTORY_CONNECTOR).build());
 }
--- src/main/java/com/galaxy/jjj/block/inventory_connector/InventoryConnectorEntity.java
+++ src/main/java/com/galaxy/jjj/block/inventory_connector/InventoryConnectorEntity.java
@@ -1,12 +1,13 @@
 package com.galaxy.jjj.block.inventory_connector;
 
+import com.galaxy.jjj.block.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class InventoryConnectorEntity extends BlockEntity {
-	public InventoryConnectorEntity(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
-		super(blockEntityType, blockPos, blockState);
+	public InventoryConnectorEntity(BlockPos blockPos, BlockState blockState) {
+		super(ModBlockEntities.INVENTORY_CONNECTOR_ENTITY,blockPos, blockState);
 	}
 }
